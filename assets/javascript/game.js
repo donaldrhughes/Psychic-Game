5d2a752e9aa9a9126df4dae80165479c572a3cb5
myrean <dhughes1@uci.edu>
2018-11-06 22:52:46 -0800

    built game frame

---
+++ assets/javascript/game.js
@@ -0,0 +1,125 @@
+//GLOBAL Vars
+//------------------------------
+
+//Arrays and Vars
+
+var coins = ["bitcoin", "ether", "xrp", "lumens", "eos", "litecoin", "bcash", "iota", "cardano", "monero", "tether", "tron", "dash", "neo", "nem", "tezos", "zcash", "decred", "waves", "status"]
+var selWord = "";
+var lettersinWord = [];
+var Blanks = 0;
+var blanksandSuccesses = [];
+var wrongLetters = [];
+
+//Win Loss Counters
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+
+
+
+//Functions
+//-------------------------------
+
+function cmMain() {
+    selWord = coins[Math.floor(Math.random() * coins.length)];
+    lettersinWord = selWord.split("");
+    Blanks = lettersinWord.length;
+
+    //reset
+    guessesLeft = 9;
+    wrongLetters = [];
+    blanksandSuccesses = [];
+
+    // Loop to create blanks
+    for (var i = 0; i < Blanks; i++) {
+        blanksandSuccesses.push("_");
+    }
+
+    // Update the mainpage
+    document.getElementById("word").innerHTML = blanksandSuccesses.join(" ");
+    document.getElementById("Guesses").innerHTML = guessesLeft;
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("losses").innerHTML = losses;
+
+    //Debug
+
+    console.log(selWord);
+    console.log(Blanks);
+    console.log(lettersinWord);
+    console.log(blanksandSuccesses);
+}
+
+
+function checkLetters(letter) {
+    //Check if letters in word
+
+    var isletterinWord = false;
+
+    for (var i = 0; i < Blanks; i++) {
+        if (selWord[i] == letter) {
+            isletterinWord = true;
+            console.log(isletterinWord);
+        }
+    }
+    if (isletterinWord) {
+        for (var i = 0; i < Blanks; i++) {
+            if (selWord[i] = letter);
+        }
+    }
+    else {
+        wrongLetters.push(letter);
+        guessesLeft--
+    }
+
+    //Debug
+    console.log(blanksandSuccesses)
+
+}
+
+function roundComplete() {
+    console.log("Wins" + wins + " | Losses" + losses + " | Guesses Left")
+
+
+    document.getElementById("Guesses").innerHTML = guessesLeft;
+    document.getElementById("word").innerHTML = blanksandSuccesses.join(" ");
+    document.getElementById("wrongGuesses").innerHTML = wrongLetters.join(" ");
+
+
+    //Check if user won
+    if (lettersinWord.toString() == blanksandSuccesses.toString()) {
+        wins++;
+        alert("You Won!");
+
+        document.getElementById("wins").innerHTML = wins;
+        cmMain();
+
+    }
+
+    //Check if user lost
+    else if (guessesLeft == 0) {
+        losses++;
+        alert("You Lost!");
+
+        document.getElementById("losses").innerHTML = losses;
+
+        cmMain();
+    }
+}
+
+
+//Main 
+//-------------------------------
+
+// Starts CryptoMatch
+cmMain();
+
+
+//grabs keystroke
+document.onkeyup = function (event) {
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetters(letterGuessed);
+    roundComplete();
+
+    // Debug
+    console.log(letterGuessed);
+}
